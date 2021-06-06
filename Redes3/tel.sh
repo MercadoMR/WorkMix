c4e15b01c981d03130de79f50a505c32b991f0e6
Martin Mercado <mmercadorg@gmail.com>
2021-06-06 06:53:44 +0200

    CVS projects and checking

--- Redes3/tel.sh
+++ Redes3/tel.sh
@@ -10,18 +10,19 @@ clean_output()
     sed -i '$d' $1
 };
 
-CVSROOT='/usr/local/cvsroot'
-
-if [ -d $CVSROOT ]; 
+### CHECK IF CVS REPOSITORY EXISTS
+CVS='/usr/local/cvsroot'
+if [ -d $CVS ]; 
   then
   echo 'El repositorio de cvs fue encontrado';
 else
   echo 'El repositorio de cvs no fue encontrado :(';
   echo 'Creandolo...';
-  cvs -d $CVSROOT init;
-  export CVSROOT=$CVSROOT;
-
+  cvs -d $CVS init;
+  export CVSROOT=$CVS;
 fi
+
+### READ ROUTER'S COMMANDS AND EXECUTE THEM VIA TELNET
 
 ACTUAL=running-config.txt
 COMANDOS=comandos.txt
@@ -33,8 +34,32 @@ while read comando
       echo "$comando";
   }
 done < $COMANDOS | telnet $ROUTER > $ACTUAL
-
+#CLEAN RETRIEVED ROUTER CONFIGURATION FILE
 clean_output $ACTUAL;
 
-
+### CREATE TODAY PROJECT FOLDER INSIDE THE REPO IF DOESN'T EXIST
+FECHA=`date +"%d%m%Y"`
+TODAY_PRO="config${FECHA}"
+if ! cvs checkout $TODAY_PRO >/dev/null;
+   then
+	echo "El proyecto de hoy no existe";
+	mkdir temprep;
+	cp $ACTUAL temprep/
+	cd temprep;
+        cvs import -m "Created dir for $FECHA configs" $TODAY_PRO mercadomr release1
+	cd ..
+	rm -r temprep;
+	cvs checkout $TODAY_PRO
+fi
+### CLONE OR UPDATE LOCAL PROJECT FOLDER
+if [ ! -d $TODAY_LOC ];
+  then
+    echo "El directorio de hoy no fue encontrado";
+    echo "Clonandolo desde el repositorio";
+    cvs checkout $TODAY_LOC;
+  else
+    cd $TODAY_LOC
+    cvs update
+    cd ..
+fi
 
